f69d9ac94633a9f78e6189de69e818cfa6e7aff9
Cody Rodgers <rodgers@rdacorp.com>
2022-11-30 09:12:50 -0500

    Add some CSS class names to the generator so the styling can be tweaked if necessary

--- Brightcove.Core/EmbedGenerator/BrightcoveEmbedGenerator.cs
+++ Brightcove.Core/EmbedGenerator/BrightcoveEmbedGenerator.cs
@@ -5,11 +5,11 @@ namespace Brightcove.Core.EmbedGenerator
 {
     public class BrightcoveEmbedGenerator
     {
-        protected string iframeTemplate = "<iframe src='https://players.brightcove.net/{0}/{1}_default/index.html?{3}={2}' allowfullscreen='' allow='encrypted-media' width='{4}' height='{5}'></iframe>";
-        protected string iframeResponsiveTemplate = "<div style='position: relative; display: block; max-width: {4}px;'><div style='padding-top: {5}%;'><iframe src='https://players.brightcove.net/{0}/{1}_default/index.html?{3}={2}' allowfullscreen='' allow='encrypted-media' style='position: absolute; top: 0px; right: 0px; bottom: 0px; left: 0px; width: 100%; height: 100%;'></iframe></div></div>";
+        protected string iframeTemplate = "<div class='brightcove-media-container brightcove-media-container-iframe'><iframe src='https://players.brightcove.net/{0}/{1}_default/index.html?{3}={2}' allowfullscreen='' allow='encrypted-media' width='{4}' height='{5}'></iframe></div>";
+        protected string iframeResponsiveTemplate = "<div class='brightcove-media-container brightcove-media-container-iframe' style='position: relative; display: block; max-width: {4}px;'><div style='padding-top: {5}%;'><iframe src='https://players.brightcove.net/{0}/{1}_default/index.html?{3}={2}' allowfullscreen='' allow='encrypted-media' style='position: absolute; top: 0px; right: 0px; bottom: 0px; left: 0px; width: 100%; height: 100%;'></iframe></div></div>";
 
-        protected string jsTemplate = "<div style='width: {4}px;'><video-js data-account='{0}' data-player='{1}' data-embed='default' controls='' data-video-id='{2}' data-playlist-id='{3}' data-application-id='' width='{4}' height='{5}' class='vjs-fluid'></video-js>{6}</div>";
-        protected string jsResponsiveTemplate = "<div style='max-width: {4}px;'><style>video-js.video-js.vjs-fluid:not(.vjs-audio-only-mode) {{padding-top: {5}%;}}</style><video-js data-account='{0}' data-player='{1}' data-embed='default' controls='' data-video-id='{2}' data-playlist-id='{3}' data-application-id='' class='vjs-fluid'></video-js>{6}</div>";
+        protected string jsTemplate = "<div class='brightcove-media-container brightcove-media-container-js' style='width: {4}px;'><video-js data-account='{0}' data-player='{1}' data-embed='default' controls='' data-video-id='{2}' data-playlist-id='{3}' data-application-id='' width='{4}' height='{5}' class='vjs-fluid'></video-js>{6}</div>";
+        protected string jsResponsiveTemplate = "<div class='brightcove-media-container brightcove-media-container-js' style='max-width: {4}px;'><style>video-js.video-js.vjs-fluid:not(.vjs-audio-only-mode) {{padding-top: {5}%;}}</style><video-js data-account='{0}' data-player='{1}' data-embed='default' controls='' data-video-id='{2}' data-playlist-id='{3}' data-application-id='' class='vjs-fluid'></video-js>{6}</div>";
         protected string jsScriptTemplate = "<script src='https://players.brightcove.net/{0}/{1}_default/index.min.js'></script>";
 
         public virtual EmbedMarkup Generate(EmbedModel model)
