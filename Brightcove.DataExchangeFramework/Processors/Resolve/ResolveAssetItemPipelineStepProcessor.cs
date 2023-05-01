3e262b03d2216b70f2683e625fde62248200437b
Cody Rodgers <rodgers@rdacorp.com>
2023-05-01 11:32:00 -0400

    Update the pull pipelines to support creating items in languages other an "en"

--- Brightcove.DataExchangeFramework/Processors/Resolve/ResolveAssetItemPipelineStepProcessor.cs
+++ Brightcove.DataExchangeFramework/Processors/Resolve/ResolveAssetItemPipelineStepProcessor.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Brightcove.DataExchangeFramework.SearchResults;
 using Brightcove.DataExchangeFramework.Settings;
@@ -7,14 +8,18 @@ using Sitecore.ContentSearch;
 using Sitecore.ContentSearch.SearchTypes;
 using Sitecore.Data;
 using Sitecore.Data.Items;
+using Sitecore.DataExchange;
 using Sitecore.DataExchange.Contexts;
+using Sitecore.DataExchange.DataAccess;
 using Sitecore.DataExchange.Local.Extensions;
+using Sitecore.DataExchange.Models;
 using Sitecore.DataExchange.Providers.Sc.DataAccess.Readers;
 using Sitecore.DataExchange.Providers.Sc.Plugins;
 using Sitecore.DataExchange.Providers.Sc.Processors.PipelineSteps;
 using Sitecore.DataExchange.Repositories;
 using Sitecore.Services.Core.Diagnostics;
 using Sitecore.Services.Core.Model;
+using Sitecore.DataExchange.Providers.Sc.Extensions;
 
 namespace Brightcove.DataExchangeFramework.Processors
 {
@@ -36,6 +41,7 @@ namespace Brightcove.DataExchangeFramework.Processors
                 Sitecore.DataExchange.Context.Plugins.Add(pipelineContext.CurrentPipelineStep.GetPlugin<ResolveAssetItemSettings>());
             }
 
+            string language = pipelineContext.GetPlugin<SelectedLanguagesSettings>()?.Languages?.FirstOrDefault() ?? "en";
             string parentItemPath = GetAssetParentItemPath(pipelineContext);
             string parentItemMediaPath = GetAssetParentItemMediaPath(pipelineContext);
 
@@ -48,27 +54,18 @@ namespace Brightcove.DataExchangeFramework.Processors
             }
 
             string fieldName = valueReader.FieldName;
-            string convertedValue = this.ConvertValueForSearch(value);
+            string convertedValue = value.ToString();//this.ConvertValueForSearch(value);
 
             if (BucketManager.IsBucket(parentItem))
             {
                 IProviderSearchContext searchContext = ContentSearchManager.GetIndex($"sitecore_{repository.DatabaseName}_index").CreateSearchContext();
 
                 //Since we must search the index becasue the target folder is a bucket the items must have a field called 'ID' that can be used to identify them
-                AssetSearchResult searchResult = searchContext.GetQueryable<AssetSearchResult>().FirstOrDefault(x => x.Path.Contains(parentItemMediaPath) && x.ID == convertedValue);
+                AssetSearchResult searchResult = searchContext.GetQueryable<AssetSearchResult>().FirstOrDefault(x => x.Path.Contains(parentItemMediaPath) && x.ID == convertedValue && x.Language == language);
                 return searchResult?.GetItem()?.GetItemModel();
             }
 
             return parentItem.Children?.Where(c => c[fieldName] == convertedValue)?.FirstOrDefault()?.GetItemModel();
-        }
-
-        protected override Guid GetParentItemIdForNewItem(
-          IItemModelRepository repository,
-          ResolveSitecoreItemSettings settings,
-          PipelineContext pipelineContext,
-          ILogger logger)
-        {
-            return Sitecore.Context.ContentDatabase.GetItem(GetAssetParentItemPath(pipelineContext)).ID.Guid;
         }
 
         private string GetAssetParentItemPath(PipelineContext context)
@@ -85,6 +82,122 @@ namespace Brightcove.DataExchangeFramework.Processors
             var accountItem = Sitecore.Context.ContentDatabase.GetItem(settings.AcccountItemId);
 
             return accountItem.Paths.MediaPath + "/" + settings.RelativePath;
+        }
+
+        protected override Guid GetParentItemIdForNewItem(IItemModelRepository repository, ResolveSitecoreItemSettings settings, PipelineContext pipelineContext, ILogger logger)
+        {
+            return Sitecore.Context.ContentDatabase.GetItem(GetAssetParentItemPath(pipelineContext)).ID.Guid;
+        }
+
+        public override object CreateNewObject(object identifierValue, PipelineStep pipelineStep, PipelineContext pipelineContext, ILogger logger)
+        {
+            if (identifierValue == null)
+                throw new ArgumentException("The value cannot be null.", nameof(identifierValue));
+            Endpoint endpoint = this.GetEndpoint(pipelineStep, pipelineContext, logger);
+            if (endpoint == null)
+                throw new ArgumentNullException("endpoint");
+            if (pipelineStep == null)
+                throw new ArgumentNullException(nameof(pipelineStep));
+            if (pipelineContext == null)
+                throw new ArgumentNullException(nameof(pipelineContext));
+            IItemModelRepository repositoryFromEndpoint = this.GetItemModelRepositoryFromEndpoint(endpoint);
+            if (repositoryFromEndpoint == null)
+                return (object)null;
+            ResolveSitecoreItemSettings sitecoreItemSettings = pipelineStep.GetResolveSitecoreItemSettings();
+            if (sitecoreItemSettings == null)
+                return (object)null;
+            ItemModel newItem = this.CreateNewItem(this.GetIdentifierObject(pipelineStep, pipelineContext, logger), repositoryFromEndpoint, sitecoreItemSettings, pipelineContext, logger);
+            this.SetRepositoryStatusSettings(RepositoryObjectStatus.DoesNotExist, pipelineContext);
+            return (object)newItem;
+        }
+
+        private IItemModelRepository GetItemModelRepositoryFromEndpoint(Endpoint endpoint)
+        {
+            return endpoint.GetItemModelRepositorySettings()?.ItemModelRepository;
+        }
+
+        private ItemModel CreateNewItem(object identifierObject, IItemModelRepository repository, ResolveSitecoreItemSettings settings, PipelineContext pipelineContext, ILogger logger)
+        {
+            IValueReader valueReader = this.GetValueReader(settings.ItemNameValueAccessor);
+            if (valueReader == null)
+                return (ItemModel)null;
+
+            DataAccessContext context = new DataAccessContext();
+            string validItemName = this.ConvertValueToValidItemName(this.ReadValue(identifierObject, valueReader, context), pipelineContext, logger);
+            if (validItemName == null)
+                return (ItemModel)null;
+
+            string language = pipelineContext.GetPlugin<SelectedLanguagesSettings>()?.Languages?.FirstOrDefault() ?? "en";
+
+            Guid itemIdForNewItem = this.GetParentItemIdForNewItem(repository, settings, pipelineContext, logger);
+
+            if (settings.DoNotCreateItemIfDoesNotExist)
+            {
+                ItemModel itemModel = new ItemModel();
+                itemModel.Add("ItemName", (object)validItemName);
+                itemModel.Add("TemplateID", (object)settings.TemplateForNewItem);
+                itemModel.Add("ParentID", (object)itemIdForNewItem);
+                itemModel.Add("ItemLanguage", (object)language);
+                return itemModel;
+            }
+
+            Guid id = repository.Create(validItemName, settings.TemplateForNewItem, itemIdForNewItem, language);
+
+            return repository.Get(id, language);
+        }
+
+        private string ConvertValueToValidItemName(
+          object value,
+          PipelineContext pipelineContext,
+          ILogger logger)
+        {
+            if (value == null)
+                return (string)null;
+            string str = value.ToString();
+            SitecoreItemUtilities plugin = Context.GetPlugin<SitecoreItemUtilities>();
+            if (plugin == null)
+            {
+                this.Log(new Action<string>(logger.Error), pipelineContext, "No plugin is specified on the context to determine whether or not the specified value is a valid item name. The original value will be used.", new string[1]
+                {
+          "missing plugin: " + typeof (SitecoreItemUtilities).FullName
+                });
+                return str;
+            }
+            if (plugin.IsItemNameValid == null)
+            {
+                this.Log(new Action<string>(logger.Error), pipelineContext, "No delegate is specified on the plugin that can determine whether or not the specified value is a valid item name. The original value will be used.", new string[3]
+                {
+          "plugin: " + typeof (SitecoreItemUtilities).FullName,
+          "delegate: IsItemNameValid",
+          "original value: " + str
+                });
+                return str;
+            }
+            if (plugin.IsItemNameValid(str))
+                return str;
+            if (plugin.ProposeValidItemName != null)
+                return plugin.ProposeValidItemName(str);
+            logger.Error("No delegate is specified on the plugin that can propose a valid item name. The original value will be used. (plugin: {0}, delegate: {1}, original value: {2})", (object)typeof(SitecoreItemUtilities).FullName, (object)"ProposeValidItemName", (object)str);
+            return str;
+        }
+
+        private IValueReader GetValueReader(IValueAccessor config) => config?.ValueReader;
+
+        private object ReadValue(object source, IValueReader reader, DataAccessContext context)
+        {
+            if (reader == null)
+                return (object)null;
+            ReadResult readResult = reader.Read(source, context);
+            return !readResult.WasValueRead ? (object)null : readResult.ReadValue;
+        }
+
+        protected override object ConvertValueToIdentifier(
+          object identifierValue,
+          PipelineStep pipelineStep,
+          PipelineContext pipelineContext,
+          ILogger logger)
+        {
+            return identifierValue;
         }
     }
 }
--- Brightcove.DataExchangeFramework/Processors/Update/UpdateVideoItemProcessor.cs
+++ Brightcove.DataExchangeFramework/Processors/Update/UpdateVideoItemProcessor.cs
@@ -38,8 +38,10 @@ namespace Brightcove.DataExchangeFramework.Processors
                 var webApiSettings = GetPluginOrFail<WebApiSettings>(endpointSettings.BrightcoveEndpoint);
                 itemModelRepository = GetPluginOrFail<ItemModelRepositorySettings>(endpointSettings.SitecoreEndpoint).ItemModelRepository;
                 service = new BrightcoveService(webApiSettings.AccountId, webApiSettings.ClientId, webApiSettings.ClientSecret);
-                ItemModel item = (ItemModel)this.GetObjectFromPipelineContext(mappingSettings.TargetObjectLocation, pipelineContext, logger);
                 Video model = (Video)this.GetObjectFromPipelineContext(mappingSettings.SourceObjectLocation, pipelineContext, logger);
+
+                string itemLanguage = pipelineContext.GetPlugin<SelectedLanguagesSettings>()?.Languages?.FirstOrDefault() ?? "en";
+                ItemModel item = (ItemModel)this.GetObjectFromPipelineContext(mappingSettings.TargetObjectLocation, pipelineContext, logger);
 
                 foreach (IMappingSet mappingSet in mappingSettings.ModelMappingSets)
                 {
@@ -64,7 +66,7 @@ namespace Brightcove.DataExchangeFramework.Processors
                     LogDebug($"Updated the video item '{item.GetItemId()}'");
                 }
 
-                UpdateVariants(mappingSettings.VariantMappingSets, item, model);
+                UpdateVariants(mappingSettings.VariantMappingSets, item, model, itemLanguage);
             }
             catch(Exception ex)
             {
@@ -73,13 +75,13 @@ namespace Brightcove.DataExchangeFramework.Processors
         }
 
 
-        private void UpdateVariants(IEnumerable<IMappingSet> mappingSets, ItemModel videoItem, Video video)
+        private void UpdateVariants(IEnumerable<IMappingSet> mappingSets, ItemModel videoItem, Video video, string itemLanguage)
         {
             var videoVariants = service.GetVideoVariants(video.Id);
 
             foreach(VideoVariant videoVariant in videoVariants)
             {
-                ItemModel videoVariantItem = ResolveVideoVariant(videoItem, videoVariant);
+                ItemModel videoVariantItem = ResolveVideoVariant(videoItem, videoVariant, itemLanguage);
                 ApplyMappings(mappingSets, videoVariant, videoVariantItem);
                 UpdateVideoVariant(videoVariantItem);
             }
@@ -102,14 +104,14 @@ namespace Brightcove.DataExchangeFramework.Processors
             }
         }
 
-        private ItemModel ResolveVideoVariant(ItemModel videoItem, VideoVariant videoVariant)
+        private ItemModel ResolveVideoVariant(ItemModel videoItem, VideoVariant videoVariant, string itemLanguage)
         {
-            ItemModel variantItem = itemModelRepository.GetChildren(videoItem.GetItemId()).Where(c => c["Language"].ToString() == videoVariant.Language).FirstOrDefault();
+            ItemModel variantItem = itemModelRepository.GetChildren(videoItem.GetItemId(), itemLanguage).Where(c => c["Language"].ToString() == videoVariant.Language).FirstOrDefault();
 
             if (variantItem == null)
             {
-                Guid variantItemId = itemModelRepository.Create(ItemUtil.ProposeValidItemName(videoVariant.Name), new Guid("{A7EAF4FD-BCF3-4511-9E8C-2ED0B165F1D6}"), videoItem.GetItemId());
-                variantItem = itemModelRepository.Get(variantItemId);
+                Guid variantItemId = itemModelRepository.Create(ItemUtil.ProposeValidItemName(videoVariant.Name), new Guid("{A7EAF4FD-BCF3-4511-9E8C-2ED0B165F1D6}"), videoItem.GetItemId(), itemLanguage);
+                variantItem = itemModelRepository.Get(variantItemId, itemLanguage);
             }
 
             return variantItem;
