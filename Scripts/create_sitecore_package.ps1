c36bc9383b3424585fe5533a52d33a20f0256455
Cody Rodgers <rodgers@rdacorp.com>
2023-05-15 12:59:07 -0400

    Update sitecore package creation script

--- Scripts/create_sitecore_package.ps1
+++ Scripts/create_sitecore_package.ps1
@@ -1,7 +1,7 @@
 # Create a new Sitecore Package (SPE cmdlet)
 $pkg = New-Package "Brightcove Video Connect for Sitecore"
 
-$pkg.Metadata.Version = "10.1.3.2"
+$pkg.Metadata.Version = "10.1.X-3.2"
 $pkg.Metadata.Publisher = "Brightcove Inc."
 
 $pkg.Metadata.Readme = "PATCHING INSTRUCTIONS:`nIf you have already installed the Brightcove connector and are now installing a patch`nthen please follow these instructions to ensure all changes inlcuded in the patch`nare properly installed.`n`n1. Select the overwrite option if prompted about item/file conflicts`n2. Delete and then re-create all pre-existing Brightcove account folders`n3. Delete and then re-create all pre-existing Brightcove DEF tenants"
@@ -25,6 +25,9 @@ $source = New-FileSource -Name "Analytics Renderings" -Root "$AppPath\sitecore\s
 $pkg.Sources.Add($source);
 
 $source = New-FileSource -Name "Embed Media RTE Controls" -Root "$AppPath\sitecore\shell\Controls\Rich Text Editor\EmbedMedia"
+$pkg.Sources.Add($source);
+
+$source = New-FileSource -Name "Embed Media Link RTE Controls" -Root "$AppPath\sitecore\shell\Controls\Rich Text Editor\EmbedLink"
 $pkg.Sources.Add($source);
 
 $source = New-FileSource -Name "Brighcove Module" -Root "$AppPath\sitecore modules\Web\Brightcove"
